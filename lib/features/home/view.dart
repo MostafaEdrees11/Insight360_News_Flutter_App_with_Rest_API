1fd93a3b11e384f9f9dc39137ec98e10a6825eeb
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-29 19:45:23 +0200

    feat(HomeScreen): implement the 2nd half of the HomeScreen
    - use ListView to build a list of news from the new_item widget.
    - we pass to ListView a list of news that has news details.
    - each new should have image, title, description, date.

--- lib/features/home/view.dart
+++ lib/features/home/view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:insight360_project/core/resources/app_colors.dart';
 import 'package:insight360_project/core/resources/app_images.dart';
 import 'package:insight360_project/features/widgets/elvated_button.dart';
+import 'package:insight360_project/features/widgets/new_item.dart';
 
 class NewsCategoryDetails {
   String content;
@@ -10,6 +11,15 @@ class NewsCategoryDetails {
   Color contentColor;
 
   NewsCategoryDetails(this.content, this.backgroundColor, this.contentColor);
+}
+
+class NewDetails {
+  String image;
+  String title;
+  String description;
+  String date;
+
+  NewDetails(this.image, this.title, this.description, this.date);
 }
 
 class HomeScreen extends StatelessWidget {
@@ -20,6 +30,57 @@ class HomeScreen extends StatelessWidget {
     NewsCategoryDetails("Politic", Colors.white, Colors.black),
     NewsCategoryDetails("Sport", Colors.white, Colors.black),
     NewsCategoryDetails("Education", Colors.white, Colors.black),
+  ];
+
+  List<NewDetails> news = [
+    NewDetails(
+      AppImages.rect1Image,
+      "New1",
+      "Chhattisgarh Polls: Ex Cong \nMLA Blames TS Deo For Losing \nPower",
+      "June 13,2024",
+    ),
+    NewDetails(
+      AppImages.rect2Image,
+      "New2",
+      "CM Announcement: Rajasthan \nBJP Chief on Delay in Govt \nFormation",
+      "June 12,2024",
+    ),
+    NewDetails(
+      AppImages.rect3Image,
+      "New3",
+      "Chhattisgarh Polls: Ex Cong \nMLA Blames TS Deo For Losing \nPower",
+      "June 11,2024",
+    ),
+    NewDetails(
+      AppImages.rect4Image,
+      "New4",
+      "CM Announcement: Rajasthan \nBJP Chief on Delay in Govt \nFormation",
+      "June 10,2024",
+    ),
+    NewDetails(
+      AppImages.rect1Image,
+      "New1",
+      "Chhattisgarh Polls: Ex Cong \nMLA Blames TS Deo For Losing \nPower",
+      "June 13,2024",
+    ),
+    NewDetails(
+      AppImages.rect2Image,
+      "New2",
+      "CM Announcement: Rajasthan \nBJP Chief on Delay in Govt \nFormation",
+      "June 12,2024",
+    ),
+    NewDetails(
+      AppImages.rect3Image,
+      "New3",
+      "Chhattisgarh Polls: Ex Cong \nMLA Blames TS Deo For Losing \nPower",
+      "June 11,2024",
+    ),
+    NewDetails(
+      AppImages.rect4Image,
+      "New4",
+      "CM Announcement: Rajasthan \nBJP Chief on Delay in Govt \nFormation",
+      "June 10,2024",
+    ),
   ];
 
   @override
@@ -127,7 +188,39 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 8),
+            SizedBox(height: 16),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "News For You",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    "Show More",
+                    style: TextStyle(
+                      color: AppColors.secondaryColor,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 4),
+            Expanded(
+              child: ListView.builder(
+                itemCount: news.length,
+                itemBuilder: (context, index) => NewItem(
+                  newImage: news[index].image,
+                  newTitle: news[index].title,
+                  newDescription: news[index].description,
+                  newDate: news[index].date,
+                ),
+              ),
+            ),
           ],
         ),
       ),
