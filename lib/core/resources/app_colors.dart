4065d700c6e9675846811c4497aad73e8b2ece07
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-29 16:50:27 +0200

    build: prepare the assets for the project
    - download the images need in the assets from the figma design.
    - configure these images in a shared file in resources.
    - configure the primary & secondary colors for the project.

---
+++ lib/core/resources/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primaryColor = Color(0xFFFFA500);
+  static final secondaryColor = Color(0xFF2C57F0);
+}
---
+++ lib/core/resources/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  /* Images */
+  static final fieldImage = "assets/images/Field.png";
+  static final rect1Image = "assets/images/Rectangle 1.png";
+  static final rect2Image = "assets/images/Rectangle 2.png";
+  static final rect3Image = "assets/images/Rectangle 3.png";
+  static final rect4Image = "assets/images/Rectangle 4.png";
+
+  /* Icons */
+}
